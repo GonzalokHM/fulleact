316520af022ce83910ed450a3b44c29ec4ed571d
Gonzalo <gonzalohm93@gmail.com>
2025-03-31 12:44:51 +0200

    build fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 })
